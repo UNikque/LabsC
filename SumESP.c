e05bdabe63b0f0d9598c09f5b14cd6744b540824
UNiqkue <34692256+UNiqkue@users.noreply.github.com>
2018-02-02 22:19:09 +0200

    Update SumESP.c

--- SumESP.c
+++ SumESP.c
@@ -2,36 +2,36 @@
 #include <stdlib.h>
 #include <math.h>
 
-/*"Find the sum of terms of the series S = 1 + 1/2 + 1/4 + 1/8 + ... while more ÅÐS" */
+/*"Find the sum of terms of the series S = 1 + 1/2 + 1/4 + 1/8 + ... while more EPS" */
 
 int main(int argc, char* argv[])
 {       
-	int n = 1;
+    int n = 1;
     float eps, x = 1.0, sum = 0;
     
-	do 
-	{
-	    printf("Input number ESP: \n"); 
+    do 
+    {
+        printf("Input number ESP: \n"); 
     	scanf("%f", &eps);
     
     	if ((eps <= 0) || (eps >= 1)) 
-		{
+	{
         	printf("Error\n");
     	}
-	} while ((eps <= 0) || (eps >= 1));
+    } while ((eps <= 0) || (eps >= 1));
     
     while ( 1.0/x>=eps) 
-	{
+    {
         sum=sum+1.0/x;    
         x*=2;             
     }
     
-	printf("Sum = %f", sum);
+    printf("Sum = %f", sum);
 
     getch();
     return 0;
 }
-//---------------------------------------------------------------------------
+
 
 
 
