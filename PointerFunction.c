acb294f6eac686caba5c631575a413d14d300c1e
UNiqkue <34692256+UNiqkue@users.noreply.github.com>
2018-02-07 00:14:47 +0200

    Update PointerFunction.c

--- PointerFunction.c
+++ PointerFunction.c
@@ -25,30 +25,30 @@ int main(int argc, char* argv[])
 
 void Division(double m, double n, double *res) 
 {
-	printf("Input natural numbers M and N:\n");
-	scanf("%lf %lf", &m, &n);
+    printf("Input natural numbers M and N:\n");
+    scanf("%lf %lf", &m, &n);
     *res = m / n;
     printf("Result of division is %lf", *res);
 }
 
 void Rounding(int i, double *res) 
 {
-	char mass[6];
-	double y;
-	*res=y;
-	adoub=modf(y, &aint);
+    char mass[6];
+    double y;
+    *res=y;
+    adoub=modf(y, &aint);
     adoub*=1000000;
     adoub = (int) adoub;
     i=adoub;
     itoa (i, mass, 10);
     printf("\nDecimal part is %s\n", mass);
     if ((mass[0]==mass[1]) && (mass[2]==mass[3]) && (mass[1]==mass[3])) 
-	    printf("The Period is %lf", round(i/100000));
-      else if ((mass[1]==mass[2]) && (mass[3]==mass[4]) && (mass[2]==mass[3])) 
-	           printf("After first symbol Period is %d", mass[2]); 
+	   printf("The Period is %lf", round(i/100000));
+        else if ((mass[1]==mass[2]) && (mass[3]==mass[4]) && (mass[2]==mass[3])) 
+	            printf("After first symbol Period is %d", mass[2]); 
 	         else if ((mass[0]==mass[2]) && (mass[1]==mass[3])) 
-			          printf("The Period is %lf", round(i/10000));
-			        else if ((mass[0]==mass[3]) && (mass[1]==mass[4]) && (mass[2]==mass[5])) 
-				             printf("The Period is %lf", round(i/1000));
+			     printf("The Period is %lf", round(i/10000));
+			    else if ((mass[0]==mass[3]) && (mass[1]==mass[4]) && (mass[2]==mass[5])) 
+				        printf("The Period is %lf", round(i/1000));
 }
 
