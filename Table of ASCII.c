3701611ae37d784984850dcb2478632f34acf0e7
UNiqkue <34692256+UNiqkue@users.noreply.github.com>
2018-03-20 23:14:38 +0200

    Update Table of ASCII.c

--- Table of ASCII.c
+++ Table of ASCII.c
@@ -1,12 +1,9 @@
-#pragma hdrstop
-#pragma argsused
 #include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-/*Недосекин Никита 51392 Вариант 21
-Лабораторная работа №9
-Задание 1: "Вывести таблицу ASCII " */
+
+/* "Output the table of ASCII " */
 
 int main(int argc, char* argv[]) {
   setlocale(LC_ALL, ".1251");
@@ -15,7 +12,7 @@ int main(int argc, char* argv[]) {
     for (j=0; j<256; j++) {
     	printf("%3d - %c \n", i, i);
     	i++;
-    	//if (i==255) break;
+   
 	}
 
     getch();
