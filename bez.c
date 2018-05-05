46b91b5d844bb38cbf80108d4c630e38e771a4fd
UNiqkue <34692256+UNiqkue@users.noreply.github.com>
2018-05-05 22:56:12 +0200

    Add files via upload

---
+++ bez.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+  int a[5]; // объявлен массив a из 5 элементов
+  int i;
+  // Ввод элементов массива
+  for (i = 0; i<5; i++) 
+  {
+    printf("a[%d] = ", i);
+    scanf("%d", &a[i]); // &a[i] - адрес i-го элемента массива
+  }
+  // Вывод элементов массива
+  for (i = 0; i<5; i++)
+    printf("%d ", a[i]); // пробел в формате печати обязателен
+  getchar(); getchar();
+  return 0;
+}
